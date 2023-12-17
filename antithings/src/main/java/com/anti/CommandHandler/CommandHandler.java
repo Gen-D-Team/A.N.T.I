d39753c52a22ecb31d48129d5051f6989b1c6ba3
acyshoang <trunghoang1124@gmail.com>
2023-12-17 22:29:13 +0700

    updated

--- antithings/src/main/java/com/anti/CommandHandler/CommandHandler.java
+++ antithings/src/main/java/com/anti/CommandHandler/CommandHandler.java
@@ -6,10 +6,20 @@ import java.util.Set;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Creeper;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.TNTPrimed;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 
 
-public class CommandHandler implements CommandExecutor {
+public class CommandHandler implements CommandExecutor, Listener {
     // Created a stored for ban items
     private  Set<Material> bannedItems = new HashSet<>();
 
@@ -27,5 +37,37 @@ public class CommandHandler implements CommandExecutor {
             }
         }
         return false;
+    }
+    
+    public void removeBannnedItems() {
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            player.getInventory().forEach(itemStack -> {
+                if (itemStack != null && bannedItems.contains(itemStack.getType())) {
+                    player.getInventory().remove(itemStack);
+                }
+            });
+        }
+    }
+
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent event) {
+        if (bannedItems.contains(event.getBlock().getType())) {
+            event.setCancelled(true);
+            event.getPlayer().sendMessage(ChatColor.RED + " You cannot place " + event.getBlock().getType().name());
+        }
+    }
+
+    @EventHandler
+    public void onEntityPlace(EntityExplodeEvent event) {
+        if (event.getEntity() instanceof TNTPrimed && bannedItems.contains(Material.TNT)) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onCreatureSpawnEvent(CreatureSpawnEvent event) {
+        if (event.getEntity() instanceof Creeper) {
+            event.setCancelled(true);
+        }
     }
 }
--- antithings/src/main/java/com/anti/Plugin.java
+++ antithings/src/main/java/com/anti/Plugin.java
@@ -1,22 +1,30 @@
 package com.anti;
 
 import java.util.logging.Logger;
+
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import com.anti.CommandHandler.CommandHandler;
 
 /*
  * antithings java plugin
  */
-public class Plugin extends JavaPlugin
-{
-  private static final Logger LOGGER=Logger.getLogger("antithings");
+public class Plugin extends JavaPlugin implements Listener {
+  private static final Logger LOGGER = Logger.getLogger("antithings");
+
+  private CommandHandler ban;
 
   public void onEnable()
   {
+    ban = new CommandHandler();
+
+    getServer().getPluginManager().registerEvents(ban, this);
     LOGGER.info("antithings enabled");
   }
 
-  public void onDisable()
-  {
+  public void onDisable() {
+    ban.removeBannnedItems();
     LOGGER.info("antithings disabled");
   }
 }
