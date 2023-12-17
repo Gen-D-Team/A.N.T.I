245a9ff76d3fc78cd1ab8336d3df106676b55751
acyshoang <trunghoang1124@gmail.com>
2023-12-18 02:40:13 +0700

    Finished

--- antithings/src/main/java/com/anti/anti.java
+++ antithings/src/main/java/com/anti/anti.java
@@ -1,12 +1,17 @@
 package com.anti;
 
 
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Creeper;
 import org.bukkit.entity.TNTPrimed;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.event.entity.EntityPlaceEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 public class anti implements Listener{
     
@@ -23,6 +28,16 @@ public class anti implements Listener{
     public void onCreatureSpawnEvent (CreatureSpawnEvent event) {
         if (event.getEntity() instanceof Creeper) {
             event.setCancelled(true);
+        }
+    }
+    // Farm Protection Events
+    @EventHandler
+    public void onPlayerInteractEvent (PlayerInteractEvent event) {
+        if (event.getAction() == Action.PHYSICAL) {
+            Block block = event.getClickedBlock();
+            if (block != null && block.getType() == Material.FARMLAND) {
+                event.setCancelled(true);
+            }
         }
     }
 }
